32312eecf18cabf39cf6e5a923e2f742f7282bbb
Hossam Eldien Mahmoud <h.m1452@icloud.com>
2019-10-29 23:06:11 +0200

    GUI and it's integration with algoritm

---
+++ GUI.py
@@ -0,0 +1,48 @@
+from tkinter import *
+import tkinter as tk
+from algorithm import  *
+import tkinter.scrolledtext as scrolledtext
+
+
+def submit():
+  output.delete('1.0',END)
+  input1 = textbox.get("1.0",END)
+  scanner(input1)
+  x = open("output.txt", "r")
+  output.insert(INSERT, x.read())
+
+def ex_fn():
+    quit()
+
+def clear():
+    textbox.delete('1.0',END)
+    output.delete('1.0',END)
+
+
+def restart():
+    output.delete('1.0',END)
+    #textbox.delete('1.0',END)
+
+window = Tk()
+
+window.title('TINY Scanner')
+window.configure(background='dodgerblue')
+window.geometry('500x500+450+100')
+
+Label(window, text='TINY code', fg='black', bg='white', font=('comicsans', 14)).place(x=20,y=2)
+
+textbox = scrolledtext.ScrolledText(master = window,undo=True,height=40)
+textbox.place(x=20,y=30)
+
+Button(command = submit ,text ="Submit",bg='springgreen').place(x=615,y=675)
+Button(command = clear ,text ="Clear",bg='gray64').place(x=574,y=675)
+
+Label(window, text='Output', fg='black', bg='white', font=('comicsans', 14)).place(x=700,y=2)
+
+output = scrolledtext.ScrolledText(master = window,undo=True,height=40)
+output.place(x=700,y=30)
+
+Button(command = restart ,text ="Restart",bg='yellow2').place(x=1260,y=675)
+Button(command = ex_fn ,text ="EXIT",bg='red2').place(x=1310,y=675)
+
+window.mainloop()
--- algorithm.py
+++ algorithm.py
@@ -2,89 +2,90 @@ import  re
 
 keyword = ["if", "read", "then", "else", "write", "repeat", "until", "end"]
 symbols = ["+", "-", "*", "/", ":", "=", "<", "(", ")", ";"]
+def scanner(string):
+        #read_file = open("code.txt","r")
+        write_file = open("output.txt","w+")
+        #string = read_file.read()
+        comment_flag = True
+        string += " "
+        token = ""
+        iterator = -1
+        for i in string :
+            iterator+=1
+            if i is '{':
+                comment_flag = False
+            if comment_flag is True :
+                if  re.match(("[a-z|A-Z]" ),i):
+                    token += i
+                    if re.match(("[a-z|A-Z]" ),string[iterator+1]):
+                       continue
+                    elif string[iterator+1] in symbols or string[iterator+1] is " " or string[iterator+1] is '\t'or string[iterator+1] is '\n':
+                       if token in keyword :
+                          write_file.write(token)
+                          write_file.write(" , ")
+                          write_file.write(token.upper())
+                          write_file.write("\n")
+                          token=''
+                       else:
+                           write_file.write(token)
+                           write_file.write(" , identifier\n")
+                           token = ''
+                    else:
+                        write_file.write("ERROR")
+                        return
 
-read_file = open("code.txt","r")
-write_file = open("output.txt","w+")
-string = read_file.read()
-comment_flag = True
-string += " "
-token = ""
-iterator = -1
-for i in string :
-    iterator+=1
-    if i is '{':
-        comment_flag = False
-    if comment_flag is True :
-        if  re.match(("[a-z|A-Z]" ),i):
-            token += i
-            if re.match(("[a-z|A-Z]" ),string[iterator+1]):
-               continue
-            elif string[iterator+1] in symbols or string[iterator+1] is " " or string[iterator+1] is '\t'or string[iterator+1] is '\n':
-               if token in keyword :
-                  write_file.write(token)
-                  write_file.write(" , ")
-                  write_file.write(token.upper())
-                  write_file.write("\n")
-                  token=''
-               else:
-                   write_file.write(token)
-                   write_file.write(" , identifier\n")
-                   token = ''
-            else:
-                write_file.write("ERROR")
-                quit()
-
-        elif  re.match(("[0-9]" ),i):
-            token += i
-            if re.match(("[0-9]"), string[iterator + 1]):
-                continue
-            elif string[iterator+1] in symbols or string[iterator+1] is " " or string[iterator+1] is '\t'or string[iterator+1] is '\n':
-                write_file.write(token)
-                write_file.write(" , number\n")
-                token = ''
-            else:
-                write_file.write("ERROR")
-                quit()
-        elif i in symbols:
-            if i is '+':
-                write_file.write(i)
-                write_file.write(" , Plus sign\n")
-            elif i is '-':
-                write_file.write(i)
-                write_file.write(" , minus sign\n")
-            elif i is '*':
-                write_file.write(i)
-                write_file.write(" , Multiply sign\n")
-            elif i is '/':
-                write_file.write(i)
-                write_file.write(" , Division sign\n")
-            elif i is ':' and string[iterator+1] is '=':
-                write_file.write(i+'=')
-                write_file.write(" , Assign\n")
-            elif i is '=' and string[iterator-1] is not ':' and iterator is not 0:
-                write_file.write(i)
-                write_file.write(" , Equal\n")
-            elif i is '<':
-                write_file.write(i)
-                write_file.write(" , Less than sign\n")
-            elif i is '(':
-                write_file.write(i)
-                write_file.write(" , left bracket\n")
-            elif i is ')':
-                write_file.write(i)
-                write_file.write(" , Right bracket\n")
-            elif i is ';':
-                write_file.write(i)
-                write_file.write(" , Semicolon\n")
-        elif i is not " " and i is not "\n":
-            write_file.write("ERROR")
-            quit()
-    elif comment_flag is False:
-        if i is '}':
-            comment_flag = True
-        else:
-            continue
-if comment_flag is False: #openning an { and didn't fold it }
-    write_file.write('ERROR')
-    quit()
-
+                elif  re.match(("[0-9]" ),i):
+                    token += i
+                    if re.match(("[0-9]"), string[iterator + 1]):
+                        continue
+                    elif string[iterator+1] in symbols or string[iterator+1] is " " or string[iterator+1] is '\t'or string[iterator+1] is '\n':
+                        write_file.write(token)
+                        write_file.write(" , number\n")
+                        token = ''
+                    else:
+                        write_file.write("ERROR")
+                        return
+                elif i in symbols:
+                    if i is '+':
+                        write_file.write(i)
+                        write_file.write(" , Plus sign\n")
+                    elif i is '-':
+                        write_file.write(i)
+                        write_file.write(" , minus sign\n")
+                    elif i is '*':
+                        write_file.write(i)
+                        write_file.write(" , Multiply sign\n")
+                    elif i is '/':
+                        write_file.write(i)
+                        write_file.write(" , Division sign\n")
+                    elif i is ':' and string[iterator+1] is '=':
+                        write_file.write(i+'=')
+                        write_file.write(" , Assign\n")
+                    elif i is '=' and string[iterator-1] is not ':' and iterator is not 0:
+                        write_file.write(i)
+                        write_file.write(" , Equal\n")
+                    elif i is '<':
+                        write_file.write(i)
+                        write_file.write(" , Less than sign\n")
+                    elif i is '(':
+                        write_file.write(i)
+                        write_file.write(" , left bracket\n")
+                    elif i is ')':
+                        write_file.write(i)
+                        write_file.write(" , Right bracket\n")
+                    elif i is ';':
+                        write_file.write(i)
+                        write_file.write(" , Semicolon\n")
+                elif i is not " " and i is not "\n":
+                    write_file.write("ERROR")
+                    return
+            elif comment_flag is False:
+                if i is '}':
+                    comment_flag = True
+                else:
+                    continue
+        if comment_flag is False: #openning an { and didn't fold it }
+            write_file.write('ERROR')
+            return
+        #read_file.close()
+        write_file.close()
